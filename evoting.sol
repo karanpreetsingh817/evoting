a66f28619c42c3ae9256233c64fc47c40ada82cc
Remix Workspace <>
2022-09-10 11:01:21 +0530

    all variable convert same name and start and end vote function added

--- evoting.sol
+++ evoting.sol
@@ -1,76 +1,85 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
 contract EvotinG{
-    address public Administrator;
-    uint private Start_timming;                      //repesent timmimg when voting is started
-    uint private End_timming;                         //repesent when voting is ended
+    address public administrator;
+    uint private start_timming;                      //repesent timmimg when voting is started
+    uint private end_timming;                         //repesent when voting is ended
 
 
-    enum Votestatus{Before_start,Votestarted,Voteend} //To check voting status
-    Votestatus public status;
+    enum vote_status{before_start,vote_started,vote_end} //To check voting status
+    vote_status public status;
 
 
-    struct  Voter{
-        uint Adhaarnumber;
-        string Name;
+    struct  voter{
+        uint adhaar_number;
+        string name;
         uint age;
-        bool isVoted;
-        bool notThis;
-        uint Votedto;
+        bool is_voted;
+        bool not_This;
+        uint voted_to;
         // code for differents state and blocks can be declare and we check whether candidate belongs to that region or not
     }
    
 
-    mapping(address=>Voter) public Voters;//baad me dekhenge ki private krna hai ja nahi....and haan validate ka bhi sochna hai bhai...
+    mapping(address=>voter) public voters;//baad me dekhenge ki private krna hai ja nahi....and haan validate ka bhi sochna hai bhai...
     
 
 
     struct candidate{
-        string PartyName;
-        string PartyFlag;
-        string CandidateName;
-        uint Adhaarnumber;
-        uint CountVote;
+        string party_name;
+        string party_flag;
+        string candidate_name;
+        uint adhaar_number;
+        uint count_vote;
     }
     mapping(uint=>candidate) candidates;//where first int takes unique candidate number
 
 
 
     constructor(){
-        Administrator=msg.sender;
-        Start_timming=block.timestamp+604800; //after deploy administator has 7 days of time to do any change
-        End_timming=Start_timming+604800;
-        status=Votestatus.Before_start;
+        administrator=msg.sender;
+        status=vote_status.before_start;
         }
 
 
 
 
-    modifier isadmnistrator{
-        require(Administrator==msg.sender,"This action perform by ony administrator");
+    modifier is_admnistrator{
+        require(administrator==msg.sender,"This action perform by ony administrator");
         _;
     }
-    modifier beforestart{
-        require(status==Votestatus.Before_start,"bro i think voting is running, yeh kam to start se pehle krna tha na bhau");
-        require(block.timestamp<Start_timming,"oh ho you are late!!!");
+    modifier before_start{
+        require(status==vote_status.before_start,"bro i think voting is running, yeh kam to start se pehle krna tha na bhau");
+        require(block.timestamp<start_timming,"oh ho you are late!!!");
         _;
     }
 
 
     modifier eligible_voter{
          address t=msg.sender;
-         require(Voters[t].isVoted ==false,"hi you are already give vote to this party");
+         require(voters[t].is_voted ==false,"hi you are already give vote to this party");
         _;
     }
 
+    function start_voting() public is_admnistrator before_start{//vote started
+        status=vote_status.vote_started;
+        start_timming=block.timestamp; //after deploy administator has 7 days of time to do any change
+        end_timming=start_timming+604800;
+        
+    }
+    function vote_end() public is_admnistrator{ //vote ended
+        require(status==vote_status.vote_started && end_timming<=block.timestamp);
+        status=vote_status.vote_end; //change status to vote_end
+    }
 
-    function add_candidate(uint cn,string memory panme,string memory pflag,string memory c_name,uint adhaar) isadmnistrator beforestart public{
+
+    function add_candidate(uint cn,string memory pname,string memory pflag,string memory c_name,uint adhaar) is_admnistrator before_start public{
           
-          candidates[cn].PartyName=panme;
-          candidates[cn].PartyFlag=pflag;
-          candidates[cn].CandidateName=c_name;
-          candidates[cn].Adhaarnumber=adhaar;
-          candidates[cn].CountVote=0;
+          candidates[cn].party_name=pname;
+          candidates[cn].party_flag=pflag;
+          candidates[cn].candidate_name=c_name;
+          candidates[cn].adhaar_number=adhaar;
+          candidates[cn].count_vote=0;
 
     }
 
@@ -82,11 +91,21 @@ contract EvotinG{
 
 
 
-    function makevote (uint candidatenum)public eligible_voter {
-        require(status==Votestatus.Votestarted,"hi stop!! there is time to start a vote!!");
-        candidates[candidatenum].CountVote=candidates[candidatenum].CountVote+1;
-        Voters[msg.sender].isVoted=true;
-        Voters[msg.sender].Votedto=candidatenum;
+    function make_vote (uint candidate_num)public eligible_voter {
+        require(status==vote_status.vote_started,"hi stop!! there is time to start a vote!!");
+        voters[msg.sender].is_voted=true;
+        voters[msg.sender].voted_to=candidate_num;
+        candidates[candidate_num].count_vote=candidates[candidate_num].count_vote+1;//increase vote count of that candidate
+     }
+
+     function de_vote(uint candidate_num) public {
+         require(voters[msg.sender].is_voted==true,"this is only for thos who already voted");
+         require(status==vote_status.vote_started,"hi stop!! there is time to start a vote!!");
+         voters[msg.sender].is_voted=false;
+         voters[msg.sender].voted_to=0;
+         candidates[candidate_num].count_vote=candidates[candidate_num].count_vote-1;//decrease vote count of that candidate
+
+
      }
 
 

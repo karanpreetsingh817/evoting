576ce8815250aa5187c9b7644b5cad890dc4109c
unknown <thunderrk2001@gmail.com>
2022-09-13 14:12:14 +0530

    Initial setup

---
+++ user/util/messageTemplate.js
@@ -0,0 +1,11 @@
+module.exports = {
+    getTemplate: function() {
+        const template = {
+            from: "E-voting DAPP",
+            error: false,
+            status: null,
+            message: null,
+        }
+        return template
+    }
+}

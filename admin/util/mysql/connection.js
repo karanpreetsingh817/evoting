7ceeebe93c0e778b1171428c32244525e786648b
unknown <thunderrk2001@gmail.com>
2022-10-04 21:05:44 +0530

    Admin sign In
    UI and backend Api setup

---
+++ admin/util/mysql/connection.js
@@ -0,0 +1,47 @@
+const mysql = require("mysql")
+var db_config = {
+    host: process.env.host,
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.database,
+}
+const db = (function() {
+    var pool;
+
+    var instance = null;
+    //create different module for sql queries with promises implementation
+    function init() {
+        pool = mysql.createPool(db_config);
+    }
+    return {
+
+        initilize: () => {
+
+            if (instance == null || instance == undefined) {
+                instance = new init();
+                instance.constructor = null;
+
+            }
+
+            return instance;
+
+        },
+        callQuery: async function(param) {
+            return new Promise((resolve, reject) => {
+                pool.query(param, (e, r) => {
+                    if (e) {
+
+                        reject(e)
+                    } else
+                        resolve(r)
+                })
+            })
+        },
+        getPool: () => {
+            return pool
+
+        }
+    }
+
+})();
+module.exports = db
